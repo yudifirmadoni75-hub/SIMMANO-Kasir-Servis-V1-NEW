2cb28caed2ec46af093d520d5a00f0f22f88b125
yudifirmadoni75-hub <yudifirmadoni75@gmail.com>
2026-09-11 10:00:12 +0000

    Reset repository before reupload

--- android/app/src/main/kotlin/com/simmano/kasirservis/MainActivity.kt
+++
@@ -1,3 +0,0 @@
-package com.simmano.kasirservis
-import io.flutter.embedding.android.FlutterActivity
-class MainActivity: FlutterActivity()
--- lib/main.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'src/app.dart';
-import 'src/services/app_store.dart';
-
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await store.init();
-  runApp(const SimmanoApp());
-}
